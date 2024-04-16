bb9c45226d4b73ba4bd13fd48fc2fe49855fd04a
BanAaron <aarontbarratt@gmail.com>
2024-04-16 12:55:14 +0100

    prm filters test

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,55 @@
 <script setup>
-import {ref} from 'vue'
+import { ref, watch } from 'vue'
 
-const total = ref(0)
-const input = ref(1)
-const error = ref("")
-
-const increment = () => {
-  const parsed = parseInt(input.value)
-  if (!isNaN(parsed)) {
-    total.value += parsed
-    error.value = ""
-  } else {
-    error.value = 'input is not not a valid number'
-  }
+const regionDivisionMap = {
+  'London' : ['London 1', 'London 2'],
+  'West' : ['West 1', 'West 2']
 }
 
-const decrement = () => {
-  const parsed = parseInt(input.value)
-  if (!isNaN(parsed)) {
-    total.value -= parsed
-    error.value = ""
-  } else {
-    error.value = 'input is not not a valid number'
-  }
-}
+const selectedRegions = ref([])
+const selectedDivisions = ref([])
+const regions = ref(Object.keys(regionDivisionMap).sort())
+let filteredDivisions = [].concat(...Object.values(regionDivisionMap)).sort()
 
-const reset = () => {
-  total.value = 0
-  input.value = 1
-  error.value = ""
+watch(()=>selectedRegions.value, ()=>{
+  // if there is no region selected we want to make sure all divisions are available
+  if (selectedRegions.value.length === 0) {
+    filteredDivisions = [].concat(...Object.values(regionDivisionMap)).sort()
+  }
+
+  const cachedSelectedDivisions = selectedDivisions.value
+  selectedDivisions.value = []
+  filteredDivisions = []
+  for (const region of selectedRegions.value) {
+    if (regionDivisionMap[region]) {
+      filteredDivisions.push(...regionDivisionMap[region])
+    }
+  }
+  for (const division of cachedSelectedDivisions) {
+    for (const region of selectedRegions.value) {
+      if (regionDivisionMap[region].includes(division)) {
+        selectedDivisions.value.push(division)
+      }
+    }
+  }
+
+  filteredDivisions.sort()
+})
+
+const onHide = ()=>{
+  console.log("onHide")
+  console.log(selectedRegions.value)
+  console.log(selectedDivisions.value)
 }
 </script>
 
 <template>
   <main>
     <div id="container">
-      <h1>Count: {{ total.toLocaleString() }}</h1>
-      <div id="inputs">
-        <Button @click="decrement">-</Button>
-        <InputNumber type="number" v-model="input"/>
-        <Button @click="increment">+</Button>
-      </div>
-      <p class="error" v-if="error"> {{ error }}</p>
-      <Button @click="reset">Reset</Button>
+      <h1>region</h1>
+      <MultiSelect v-model="selectedRegions" :options="regions" @hide="onHide"/>
+      <h1>division</h1>
+      <MultiSelect v-model="selectedDivisions" :options="filteredDivisions" @hide="onHide"/>
     </div>
   </main>
 </template>
@@ -52,36 +59,9 @@ const reset = () => {
   height: 100vh;
   width: 100vw;
   display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 1rem;
-
-  h1 {
-    font-family: Inter, system-ui, serif;
-    text-align: center;
-  }
-}
-
-#inputs {
-  display: flex;
-  width: fit-content;
   flex-direction: row;
   align-items: center;
   justify-content: center;
   gap: 1rem;
-
-  button, input {
-    width: 2.5rem;
-    height: 2.5rem;
-    text-align: center;
-    justify-content: center;
-  }
-}
-
-.error {
-  color: red;
-  font-weight: bold;
-  font-family: "Fira Code", Inconsolata, monospace;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import './assets/main.css'
 import PrimeVue from 'primevue/config'
 import 'primevue/resources/themes/aura-dark-cyan/theme.css'
-import Button from 'primevue/button'
-import InputNumber from 'primevue/inputnumber'
-import {createApp} from 'vue'
+import MultiSelect from "primevue/multiselect";
+import { createApp } from 'vue'
 import App from './App.vue'
 
 
 createApp(App)
-  .use(PrimeVue)
-  .component('Button', Button)
-  .component('InputNumber', InputNumber)
-  .mount('#app')
+    .use(PrimeVue)
+    .component('MultiSelect', MultiSelect)
+    .mount('#app')
